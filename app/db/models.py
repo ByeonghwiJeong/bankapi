1be0d2777b7417b4a1b02a24d24acecd7fbbe72a
Byeonghwi Jeong <95831345+ByeonghwiJeong@users.noreply.github.com>
2023-06-11 23:29:05 +0900

    feat : database migration (mysql)
    - pymysql
    - app/db/model.py :
      - Define accounts, cards, transactions

---
+++ app/db/models.py
@@ -0,0 +1,44 @@
+from enum import Enum as PyEnum
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean, BigInteger, DateTime, Enum
+from sqlalchemy import Column
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+
+class DbAccount(Base):
+    __tablename__ = "accounts"
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(String(20), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    limit = Column(Integer, default=0)
+    balance = Column(BigInteger, default=0)
+    cards = relationship("DbCard", back_populates="account")
+
+
+class DbCard(Base):
+    __tablename__ = "cards"
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(String(20), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    is_active = Column(Boolean, default=True)
+    account_id = Column(Integer, ForeignKey("accounts.id"))
+    account = relationship("DbAccount", back_populates="cards")
+
+
+class TransactionType(PyEnum):
+    DEPOSIT = "deposit"
+    WITHDRAW = "withdraw"
+
+
+class DbTransaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Integer, nullable=False)
+    transaction_type = Column(Enum(TransactionType), nullable=False)
+    card_id = Column(Integer, ForeignKey("cards.id"))
+    card = relationship("DbCard", back_populates="transactions")
+    account_id = Column(Integer, ForeignKey("accounts.id"))
+    account = relationship("DbAccount", back_populates="transactions")
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
-
+from app.db.database import engine
+from app.db import models
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+models.Base.metadata.create_all(engine)
+
+
 def start():
-    """Starts the FastAPI server. `poetry run uvicorn app.main:start` at the root of the project."""
+    """
+    Starts the FastAPI server.
+    `poetry run uvicorn app.main:start`
+    at the root of the project.
+    """
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
