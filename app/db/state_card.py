a791638ea12704f1b074835d6c88ec2d9bbf554e
Byeonghwi Jeong <95831345+ByeonghwiJeong@users.noreply.github.com>
2023-06-12 15:07:44 +0900

    feat : Set state pattern for Card

---
+++ app/db/state_card.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+class Card:
+    def __init__(self, init_state: bool = True):
+        if init_state:
+            self.state = ActiveCard()
+        else:
+            self.state = InactiveCard()
+
+    def setState(self, state):
+        self.state = state
+
+    def status(self):
+        return self.state.status()
+
+    def change(self):
+        self.state.change_state(self)
+
+
+class State(ABC):
+    @abstractmethod
+    def status(self):
+        pass
+
+    @abstractmethod
+    def change_state(self, card_state: Card):
+        pass
+
+
+class ActiveCard(State):
+    def status(self):
+        return "Active"
+
+    def change_state(self, card_state: Card):
+        card_state.setState(InactiveCard())
+
+
+class InactiveCard(State):
+    def status(self):
+        return "Inactive"
+
+    def change_state(self, card_state: Card):
+        card_state.setState(ActiveCard())
