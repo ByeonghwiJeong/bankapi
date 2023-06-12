d57c4ad2e95b59b7f590ea6fa82f2d48c71dd858
Byeonghwi Jeong <95831345+ByeonghwiJeong@users.noreply.github.com>
2023-06-12 17:35:38 +0900

    feat : Check account balance

--- app/db/db_account.py
+++ app/db/db_account.py
@@ -5,6 +5,8 @@ from app.routers.schemes import Account
 from sqlalchemy.orm.session import Session
 from app.db.hashing import Hash
 from app.db.generate_account_num import create_unique_account_number
+from app.routers.schemes import AccountAuth
+
 
 def create_account(db: Session, request: Account):
     account = db.query(DbAccount).filter(DbAccount.username == request.username).first()
@@ -28,3 +30,8 @@ def get_account_by_username(db: Session, username: str):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
       detail=f'User with username {username} not found')
   return user
+
+
+def get_balance(db: Session, current_account: AccountAuth):
+    account = get_account_by_username(db, current_account.username)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"balance": account.balance})
--- app/routers/account.py
+++ app/routers/account.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm.session import Session
-from app.routers.schemes import Account
+from app.routers.schemes import Account, AccountAuth
 from app.db.database import get_db
+from app.auth.oauth2 import get_current_account
 from app.db import db_account
 from fastapi import APIRouter, Depends
 
@@ -10,6 +11,15 @@ router = APIRouter(
     tags=["account"],
 )
 
+
 @router.post("", response_model=Account)
 async def create_account(request: Account, db: Session = Depends(get_db)):
     return db_account.create_account(db, request)
+
+
+@router.get("/balance")
+async def get_balance(
+    db: Session = Depends(get_db),
+    current_account: AccountAuth = Depends(get_current_account),
+):
+    return db_account.get_balance(db, current_account)
